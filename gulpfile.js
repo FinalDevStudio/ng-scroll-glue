dbc8c311af1eab4061fe1a2836baf69aea4ba762
Santiago G. Marín <santiago@finaldevstudio.com>
2016-06-22 11:56:58 -0400

    Updated build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,46 @@
 'use strict';
-
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var gulp = require('gulp');
-var del = require('del');
-
-var paths = {
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const del = require('del');
+const paths = {
   scripts: 'src/*.js'
 };
-
 /**
  * Clean the dist folder.
  */
-gulp.task('clean', function (next) {
-  del('dist', next);
+gulp.task('clean', () => {
+  del.sync('dist');
 });
-
 /**
  * Minify and copy to dist folder.
  */
-gulp.task('minify', ['clean'], function () {
-  return gulp.src(paths.scripts).
-  pipe(sourcemaps.init()).
-  pipe(uglify()).
-  pipe(sourcemaps.write()).
-  pipe(rename({
-    extname: '.min.js'
-  })).
-  pipe(gulp.dest('dist/'));
+gulp.task('minify', ['clean'], () => {
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('dist/'));
 });
-
 /**
  * Beautify and copy to dist folder.
  */
-gulp.task('beautify', ['minify'], function () {
-  return gulp.src(paths.scripts).
-  pipe(uglify({
-    mangle: false,
-    compress: false,
-    output: {
-      bracketize: true,
-      indent_level: 2,
-      beautify: true
-    }
-  })).
-  pipe(gulp.dest('dist/'));
+gulp.task('beautify', ['minify'], () => {
+  return gulp.src(paths.scripts)
+    .pipe(uglify({
+      mangle: false,
+      compress: false,
+      output: {
+        bracketize: true,
+        indent_level: 2,
+        beautify: true
+      }
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build', ['beautify']);
@@ -53,7 +48,7 @@ gulp.task('build', ['beautify']);
 /**
  * Watch for file changes.
  */
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch(paths.scripts, ['build']);
 });
 
